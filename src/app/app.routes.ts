fbfe1f4eeca6978ea3e113527cdef115879addec
AbadockC <148381552+GCantanhede@users.noreply.github.com>
2023-12-14 11:14:09 +0100

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import {MenuComponent} from "./menu/menu.component";
 import {PreguntesFrecuentsComponent} from "./preguntes-frecuents/preguntes-frecuents.component";
 import {ContactosComponent} from "./contactos/contactos.component";
 import {CatalogoComponent} from "./catalogo/catalogo.component";
+import {LoginComponent} from "./login/login.component";
 export const routes: Routes = [
-  {path: 'condiciones', component:CondicionesComponent},
-  {path: '', component:MenuComponent},
-  {path: 'faq', component:PreguntesFrecuentsComponent},
-  {path: 'formulario-contactos', component:ContactosComponent},
-  {path: 'catalogo', component:CatalogoComponent}
+  {path: 'condiciones', title: 'condiciones',component:CondicionesComponent},
+  {path: '', title: '', component:MenuComponent},
+  {path: 'faq', title: 'faq', component:PreguntesFrecuentsComponent},
+  {path: 'formulario-contactos', title: 'contactos', component:ContactosComponent},
+  {path: 'catalogo', title: 'catalogo', component:CatalogoComponent},
+  {path: 'login', title: 'login', component:LoginComponent}
 
 
 
