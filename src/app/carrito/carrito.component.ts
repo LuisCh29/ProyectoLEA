3fb92fb189708014f549a21b13e60338a23ef5eb
LuisC <luis.chen@institutvidreres.cat>
2023-12-29 16:42:16 +0100

    nav fix

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    ReactiveFormsModule,
+    FormsModule
   ],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css'
 })
 export class CarritoComponent {
+
+  codigoPromocional: string = '';
+  totalCompra: number = 50;
+  totalConDescuento: number = this.totalCompra;
+
+  aplicarDescuento() {
+    if (this.codigoPromocional === '2024') {
+      this.totalConDescuento = this.totalCompra - 15;
+    } else {
+      this.totalConDescuento = this.totalCompra;
+      console.log("funciona, descuento aplicado")
+    }
+  }
 
 }
