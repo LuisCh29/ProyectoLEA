77fe2f16b64142dbee75ed490a0e03050801a6ef
AbadockC <guilherme.cantanhede@institutvidreres.cat>
2024-01-03 14:34:23 +0100

    Carrito funcionant y logica de borrar productes feta

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ProductosService} from "../productos.service";
+import {ProductosService, Producto, addProducto} from "../productos.service";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-carrito',
@@ -9,24 +10,59 @@ import {ProductosService} from "../productos.service";
   imports: [
     RouterOutlet,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgIf,
+    NgForOf,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css'
 })
+
+
 export class CarritoComponent {
-
-  totalCompra: number = 50;
+  productos: any[][] = [[], []];
+  totalCompra: number = 0;
   totalConDescuento: number = this.totalCompra;
+  codigoPromocional: string = '';
 
-  constructor(private ProductosService: ProductosService) {}
+  ngOnInit() {
+    const productosCarrito = localStorage.getItem('ProdsCar');
+    this.productos = productosCarrito ? JSON.parse(productosCarrito) : [[], []];
+    this.sumarPrecios();
+    this.aplicarDescuento();
+  }
+
+  addProducto(NomProducto: string, PreProducto: number) {
+    const nuevoProducto: Producto = {
+      nombre: NomProducto,
+      precio: PreProducto
+    };
+
+    addProducto(nuevoProducto, this.productos);
+  }
+
+  limpiarLista() {
+    localStorage.removeItem('ProdsCar');
+  }
+
+
+
+  sumarPrecios() {
+    this.totalCompra = this.productos[0].reduce((total, producto) => total + producto.precio, 0);
+  }
+
   aplicarDescuento() {
     // @ts-ignore
-    if (document.getElementById("codigoPromocional").value === '2024') {
+    if (document.getElementById('codigoPromocional').value === '2024') {
       this.totalConDescuento = this.totalCompra - 15;
+      console.log('Funciona, descuento aplicado');
     } else {
       this.totalConDescuento = this.totalCompra;
-      console.log("funciona, descuento aplicado")
     }
   }
+
 }
+
+
