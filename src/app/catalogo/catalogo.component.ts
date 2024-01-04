9d43cee3e322a1f6ac3c39a8d7b009ec210ced87
AbadockC <guilherme.cantanhede@institutvidreres.cat>
2024-01-04 13:05:25 +0100

    Cataleg final

--- src/app/catalogo/catalogo.component.ts
+++ src/app/catalogo/catalogo.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {ProductosService} from "../productos.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NgIf
   ],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.css'
 })
 export class CatalogoComponent {
 
+  ngOnInit() {
+    sessionStorage.setItem("filter",'')
+  }
+
   constructor(private ProductosService: ProductosService) {
+    sessionStorage.setItem("filter",'')
   }
 
   filter(filter: string){
     sessionStorage.setItem("filter",filter)
+    console.log(filter)
   }
 
   addProduct(product: string, precio:number){
@@ -25,4 +33,5 @@ export class CatalogoComponent {
     console.log(product + precio)
   }
 
+  protected readonly sessionStorage = sessionStorage;
 }
